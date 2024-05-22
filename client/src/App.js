d329e353e8e086c202b54dae2ef40894d54d3d28
Rishabh Bharatbhai Parmar <95970520+Rishabh3243@users.noreply.github.com>
2024-05-22 12:42:58 +0530

    gh

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PrimeReactProvider } from 'primereact/api';
+import HomePage from "./pages/HomePage";
+import VideoToLanguagePage from "./pages/VideoToLanguagePage";
 import './App.css';
 
 function App() {
   return (
+    <PrimeReactProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="VideoToLanguage" element={<VideoToLanguagePage/>}/>
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
+    </PrimeReactProvider>
   );
 }
 
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,64 @@
+import React from 'react'; 
+import { Menubar } from 'primereact/menubar';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import 'primeicons/primeicons.css';
+
+export default function NavBar() {
+
+    const items = [
+        {
+            label: 'Home',
+            icon: 'pi pi-home',
+            command:()=>{ window.location=""; }
+        },
+        {
+            label: 'Features',
+            icon: 'pi pi-star',
+            command:()=>{ window.location="VideoToLanguage"; }
+        },
+        {
+            label: 'Projects',
+            icon: 'pi pi-search',
+            items: [
+                {
+                    label: 'Components',
+                    icon: 'pi pi-bolt'
+                },
+                {
+                    label: 'Blocks',
+                    icon: 'pi pi-server'
+                },
+                {
+                    label: 'UI Kit',
+                    icon: 'pi pi-pencil'
+                },
+                {
+                    label: 'Templates',
+                    icon: 'pi pi-palette',
+                    items: [
+                        {
+                            label: 'Apollo',
+                            icon: 'pi pi-palette'
+                        },
+                        {
+                            label: 'Ultima',
+                            icon: 'pi pi-palette'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Contact',
+            icon: 'pi pi-envelope'
+        }
+    ];
+
+
+    return (
+        <div className="card">
+            <Menubar model={items} />
+        </div>
+    )
+}
+        
